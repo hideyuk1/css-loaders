50966a633aab6663e519e675a4363c3f447293fd
Lucas China <lucashideyuki@gmail.com>
2020-03-09 14:51:12 -0300

    Fix export of new loader

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -2,5 +2,6 @@ import threeDots from "./three-dots";
 import circularBars from "./circular-bars";
 import movingSquareLine from "./moving-square-line";
 import nestedCircles from "./nested-circles";
+import squareGrid from "./square-grid";
 
-export { circularBars, threeDots, movingSquareLine, nestedCircles };
+export { circularBars, threeDots, movingSquareLine, nestedCircles, squareGrid };
